d8cdcbd83501486531d082c4fdd64fe029e09e0e
Justus Beck <ponzopa@gmx.de>
2026-07-12 21:44:23 +0200

    chore(ios): scaffold Xcode project (SwiftUI, Swift Testing, iOS 18.0 target)

---
+++ ios/PintKing/PintKing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PintKing
+//
+//  Created by Justus Philips on 12/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/PintKing/PintKing/PintKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PintKingApp.swift
+//  PintKing
+//
+//  Created by Justus Philips on 12/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PintKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
